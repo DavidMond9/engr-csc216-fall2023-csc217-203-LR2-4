705d896db07fdfc1aeebbc37e2f632828d624b11
dmmond <dmmond@ncsu.edu>
2023-10-16 15:27:28 -0400

    [Implementation] Implemented CourseNameValidatorFSM and test class.

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/course/validator/CourseNameValidatorFSMTest.java
@@ -0,0 +1,12 @@
+package edu.ncsu.csc216.pack_scheduler.course.validator;
+
+/**
+ * Testing the class CourseNameValidatorFSM which is a Finite State Machine for checking whether
+ * a Course's Name is valid.
+ * 
+ * 
+ */
+public class CourseNameValidatorFSMTest {
+
+
+}
