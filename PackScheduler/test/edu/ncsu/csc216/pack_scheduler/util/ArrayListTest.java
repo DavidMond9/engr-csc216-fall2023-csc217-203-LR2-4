95c49de602ba3daa0e7be5f1b7e564d2392a4f8d
dcmunro <dcmunro@ncsu.edu>
2023-10-23 16:14:32 -0400

    test set partially implemnted

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
@@ -36,6 +36,17 @@ class ArrayListTest {
 	@Test
 	void testAdd() {
 		ArrayList<Integer> list = new ArrayList();
+		//valid checks
+		list.add(0, 1);
+		assertEquals(11, list.size(), "Size should be 1 after adding more elements");
+		assertEquals(Integer.valueOf(1), list.get(0), "check to make sure it is being added correctly");
+		// Test adding more elements to trigger array growth
+        for (int i = 2; i < 11; i++) {
+            list.add(i - 1, i);
+        }
+        assertEquals(20, list.size(), "check to see if arraylist doubles when a tenth integer is added");
+		
+		
 		//Invalid checks
 		//null pointer check
 		Exception e1 = assertThrows(NullPointerException.class, 
@@ -49,12 +60,27 @@ class ArrayListTest {
 		Exception e3 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(-1, 33));
 		assertEquals("Out of bounds", e3.getMessage(), "checking that out of bounds exception is thrown for a negative index");
 		Exception e4 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(1000, 33333));
-		assertEquals("Out of bounds", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");
-
-				
-				
+		assertEquals("Out of bounds", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");		
 	}
 	
+	/**
+	 * this will test the set method
+	 */
+	@Test void testSet() {
+	ArrayList<Integer> list = new ArrayList<>();
+    
+    // Add some elements to the list
+    for (int i = 0; i < 5; i++) {
+        list.add(i, i + 1);
+    }
+    
+    // Test setting elements
+    assertEquals(Integer.valueOf(1), list.set(0, 6), "Original element should be returned");
+    assertEquals(Integer.valueOf(6), list.get(0), "New element should be at index 0");
 	
-	
+    
+    //invalid checks
+    
+	}
+
 }
