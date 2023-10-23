ea8f4884a1d28556494c4409400fccfb5c119917
dmmond <dmmond@ncsu.edu>
2023-10-23 16:32:14 -0400

    [Skeleton] Finished skeleton and edited tests for ArrayListTest

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
@@ -22,17 +22,16 @@ class ArrayListTest {
 	private int size;
 	
 	/**
-	 * this will test the array list constructor
+	 * Testing the constructor in ArrayList.
 	 */
 	@Test
 	void testArrayList() {
 		ArrayList<Integer> list = new ArrayList();
-		//Size should be 0 after construction
-		assertEquals(0, list.size());
+		assertEquals(0, list.size(), "Size should be 0 after construction" );
 		}
 
 	/**
-	 * this will test the array list add method
+	 * Testing the add method in ArrayList.
 	 */
 	@Test
 	void testAdd() {
@@ -41,21 +40,36 @@ class ArrayListTest {
 		//null pointer check
 		Exception e1 = assertThrows(NullPointerException.class, 
 						() -> list.add(4, null));
-		assertEquals("Element cannot be null", e1.getMessage(), "checking that a null pointer exception is thrown when a null element is added");
+		assertEquals("Element cannot be null.", e1.getMessage(), "checking that a null pointer exception is thrown when a null element is added");
 		//duplicate element
 		list.add(1, 66);
 		Exception e2 = assertThrows(IllegalArgumentException.class, () -> list.add(3, 66));
-		assertEquals("Duplicate element", e2.getMessage(), "checking that a duplicate element cannot be added");
+		assertEquals("Duplicate element not allowed.", e2.getMessage(), "checking that a duplicate element cannot be added");
 		//index out of range
 		Exception e3 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(-1, 33));
-		assertEquals("Out of bounds", e3.getMessage(), "checking that out of bounds exception is thrown for a negative index");
+		assertEquals("Index out of bounds.", e3.getMessage(), "checking that out of bounds exception is thrown for a negative index");
 		Exception e4 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(1000, 33333));
-		assertEquals("Out of bounds", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");
-
-				
-				
+		assertEquals("Index out of bounds.", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");
 	}
-	
-	
-	
+	/**
+	 * Testing the remove method in ArrayList.
+	 */
+	@Test
+	void testRemove() {
+		
+	}
+	/**
+	 * Testing the set method in ArrayList.
+	 */
+	@Test
+	void testSet() {
+		
+	}
+	/**
+	 * Testing the get method in ArrayList.
+	 */
+	@Test
+	void testGet() {
+		
+	}
 }
