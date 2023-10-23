5be0e793e00401373421bf23c09a87e08b7a58d1
dmmond <dmmond@ncsu.edu>
2023-10-23 15:46:51 -0400

    [Implementation] Worked on ArrayList methods.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -23,26 +23,64 @@ public class ArrayList<E> extends AbstractList<E> {
 		size = 0;
 	}
 	
-	public void add(int idx, E list) {
+	public void add(int idx, E ele) {
+		
 		if(size == INIT_SIZE) {
 			
 		}
 		for(int i = 0; i < list.length; i++) {
 			if(i == idx) {
+				E res = list[i];
+				list[i] = ele;
+				E t = list[i+1];
+				list[i+1] = res;
+				E s = list[i+2];
+				list[i+2] = t;
+				E r = list[i+3];
+				list[i+3] = s;
+				for(int j = list.length; j < i; j--) {
+					E temp = list[j+1];
+					list[j+1] = list[j];
+					list[j] = temp;
+				}
 				
 			}
 		}
 	}
 	
 	public E remove(int idx) {
-		return null;
+		if(idx < 0 || idx >= size()) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
+		
+		for(int i = 0; i < list.length; i++) {
+			if(i == idx) {
+				if(idx == size - 1) {
+					E ele = list.remove(i);
+				}
+				else {
+					E ele = list.remove(i);
+					
+				}
+			}
+		}
+		return ele;
 	}
 	
 	public E set(int idx, E list) {
+		
+		if()
+		
+		if(idx < 0 || idx >= size()) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
 		return null;
 	}
 	
 	public E get(int idx, E list) {
+		if(idx < 0 || idx >= size()) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
 		return null;
 	}
 }
