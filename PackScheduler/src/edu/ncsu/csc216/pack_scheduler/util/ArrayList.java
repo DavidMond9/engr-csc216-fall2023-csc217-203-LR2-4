f8b60f8738d952baaca37741c5bb055a1fa1437d
dmmond <dmmond@ncsu.edu>
2023-10-23 16:02:32 -0400

    [Implementation] Finished add method.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -4,6 +4,8 @@
 package edu.ncsu.csc216.pack_scheduler.util;
 
 import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * 
@@ -15,37 +17,33 @@ public class ArrayList<E> extends AbstractList<E> {
 	
 	@SuppressWarnings("unchecked")
 	public ArrayList() {
-		//create array object
 		list = (E[]) new Object[INIT_SIZE];
-		//cast to E
-		
-		//set size to 0
 		size = 0;
 	}
 	
 	public void add(int idx, E ele) {
-		
-		if(size == INIT_SIZE) {
-			
+		if(ele == null) {
+			throw new NullPointerException("Element cannot be null.");
 		}
-		for(int i = 0; i < list.length; i++) {
-			if(i == idx) {
-				E res = list[i];
-				list[i] = ele;
-				E t = list[i+1];
-				list[i+1] = res;
-				E s = list[i+2];
-				list[i+2] = t;
-				E r = list[i+3];
-				list[i+3] = s;
-				for(int j = list.length; j < i; j--) {
-					E temp = list[j+1];
-					list[j+1] = list[j];
-					list[j] = temp;
-				}
-				
+		if(idx < 0 || idx >= size()) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
+		for(int i = 0; i < size; i++) {
+			if(Objects.equals(list[i], ele)) {
+				throw new IllegalArgumentException("Duplicate element not allowed.");
 			}
 		}
+		if(size == list.length) {
+			int newCapacity = list.length * 2;
+			list = Arrays.copyOf(list, newCapacity);
+		}
+		
+		if(idx < size) {
+			System.arraycopy(list, idx, list, idx + 1, size - idx);
+			
+		}
+		list[idx] = ele;
+		size += 1;
 	}
 	
 	public E remove(int idx) {
