bcd2aa6a3d6894cb9251f2cc803cbb286ded45eb
dmmond <dmmond@ncsu.edu>
2023-10-23 15:21:55 -0400

    [Implementation] Added method headers and constructor for ArrayList

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -3,9 +3,45 @@
  */
 package edu.ncsu.csc216.pack_scheduler.util;
 
+import java.util.AbstractList;
+
 /**
  * 
  */
-public class ArrayList {
-
+public class ArrayList<E> extends AbstractList<E> {
+	private static final int INIT_SIZE = 10;
+	private E list;
+	private int size;
+	
+	public ArrayList() {
+		//create array object
+		
+		//cast to E
+		
+		//set size to 0
+		size = 0;
+	}
+	
+	public void add(int idx, E list) {
+		if(size == INIT_SIZE) {
+			
+		}
+		for(int i = 0; i < list.length; i++) {
+			if(i == idx) {
+				
+			}
+		}
+	}
+	
+	public E remove(int idx) {
+		return null;
+	}
+	
+	public E set(int idx, E list) {
+		return null;
+	}
+	
+	public E get(int idx, E list) {
+		return null;
+	}
 }
