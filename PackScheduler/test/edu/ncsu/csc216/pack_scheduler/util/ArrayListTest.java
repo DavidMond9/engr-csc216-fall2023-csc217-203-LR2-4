dcb4323e5b645d303e89d815878f7e816933969e
Audrey Fuelleman <amfuelle@ncsu.edu>
2023-10-27 11:40:36 -0400

    [Debugging]
    Fixed ArrayList add method and test.

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayListTest.java
@@ -47,9 +47,7 @@ class ArrayListTest {
         }
         
         list.add(INIT_SIZE, "test"); //this line may not be needed once add method is fixed
-        assertEquals(20, list.size(), "check to see if arraylist doubles when a tenth integer is added");
-		
-		
+        //assertEquals(20, list.size(), "check to see if arraylist doubles when a tenth integer is added");
 
 		//assertEquals(0, list.size());
 		//Invalid checks
@@ -58,14 +56,15 @@ class ArrayListTest {
 						() -> list.add(0, null));
 		assertEquals("Element cannot be null.", e1.getMessage(), "checking that a null pointer exception is thrown when a null element is added");
 		//duplicate element
-		list.add(15, "hi");
-		Exception e2 = assertThrows(IllegalArgumentException.class, () -> list.add(65, "hi"));
+		list.add(10, "hi");
+		
+		Exception e2 = assertThrows(IllegalArgumentException.class, () -> list.add(11, "hi"));
 		assertEquals("Duplicate element not allowed.", e2.getMessage(), "checking that a duplicate element cannot be added");
 		//index out of range
 		Exception e3 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(-1, "thirty"));
 		assertEquals("Index out of bounds.", e3.getMessage(), "checking that out of bounds exception is thrown for a negative index");
 		Exception e4 = assertThrows(IndexOutOfBoundsException.class, () -> list.add(1000, "woah"));
-		assertEquals("Out of bounds", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");		
+		assertEquals("Index out of bounds.", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");		
 		assertEquals("Index out of bounds.", e4.getMessage(), "checking that out of bounds exception is thrown for a negative index");
 		
 		list.add(1, "h");
