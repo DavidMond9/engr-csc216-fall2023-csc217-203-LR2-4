3496e36e19b112cce877983b3f5dfef759a268fa
dmmond <dmmond@ncsu.edu>
2023-10-30 15:38:25 -0400

    [Documentation][Cleaning] Added javadoc to CourseRoll, fixed a few methods.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
@@ -1,36 +1,69 @@
 package edu.ncsu.csc216.pack_scheduler.course.roll;
 
 import edu.ncsu.csc216.pack_scheduler.user.Student;
+import edu.ncsu.csc216.pack_scheduler.util.LinkedAbstractList;
 
+/**
+ * CourseRoll class. Holds a 
+ * @author David Mond
+ */
 public class CourseRoll {
+	/**
+	 * Field for list of students for the roll.
+	 */
 	private LinkedAbstractList<Student> roll;
+	/**
+	 * Field for the enrollment cap.
+	 */
 	private int enrollmentCap;
+	/**
+	 * Field for the minimum capacity.
+	 */
 	private static final int MIN_ENROLLMENT = 10;
+	/**
+	 * Field for the maximum capacity.
+	 */
 	private static final int MAX_ENROLLMENT = 250;
 	
-	
+	/**
+	 * CourseRoll constructor creates a course roll. 
+	 * @param capacity the capacity to set
+	 */
 	public CourseRoll(int capacity) {
-		roll = new LinkedAbstractList<Student>();
 		setEnrollmentCap(capacity);
+		roll = new LinkedAbstractList<Student>(enrollmentCap);
 	}
 	
+	/**
+	 * Gets the open seats from the capacity - current roll size.
+	 * @return an integer of how many open seats there are.
+	 */
 	public int getOpenSeats() {
 		return enrollmentCap - roll.size();
 	}
-	
+	/**
+	 * Gets the enrollment capacity.
+	 * @return an integer of the capacity.
+	 */
 	public int getEnrollmentCap() {
 		return enrollmentCap;
 	}
-	
+	/**
+	 * Sets the enrollment capacity.
+	 * @param cap to set.
+	 */
 	public void setEnrollmentCap(int cap) {
 		if(cap < MIN_ENROLLMENT || cap > MAX_ENROLLMENT) {
 			throw new IllegalArgumentException("Invalid capacity.");
 		}
 		enrollmentCap = cap;
 	}
-	
+	/**
+	 * Adds a student to the roll.
+	 * @param s the student to add.
+	 */
 	public void enroll(Student s) {
-		if(canEnroll(s) == false) {
+		if(!canEnroll(s)) {
 			throw new IllegalArgumentException("Cannot enroll student.");
 		}
 		
@@ -40,7 +73,10 @@ public class CourseRoll {
 			throw new IllegalArgumentException("Cannot enroll student.");
 		}
 	}
-	
+	/**
+	 * Removes a student from the roll.
+	 * @param s the student to remove.
+	 */
 	public void drop(Student s) {
 		if(s == null) {
 			throw new IllegalArgumentException("Cannot drop student.");
@@ -52,9 +88,13 @@ public class CourseRoll {
 			throw new IllegalArgumentException("Cannot drop student.");
 		}
 	}
-	
+	/**
+	 * Checks to see if a student can be enrolled to the current roll.
+	 * @param s student to enroll.
+	 * @return boolean to see if it can be enrolled, true, or can't be enrolled, false.
+	 */
 	public boolean canEnroll(Student s) {
-		if(s == null || roll.size().equals(enrollmentCap)) {
+		if(s == null || roll.size() == enrollmentCap) {
 			return false;
 		}
 		for(int i = 0; i < roll.size(); i++) {
